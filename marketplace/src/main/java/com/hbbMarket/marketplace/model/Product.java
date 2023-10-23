1225e143e0d735f92c93b357fd002c17203d39b6
henriquebenjamim <henriiqqq@gmail.com>
2023-10-23 10:19:10 -0300

    Feat: simple product entity

--- marketplace/src/main/java/com/hbbMarket/marketplace/model/Product.java
+++ marketplace/src/main/java/com/hbbMarket/marketplace/model/Product.java
@@ -1,4 +1,26 @@
 package com.hbbMarket.marketplace.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity(name = "Product")
+@Table(name = "products")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of="id")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+    private Float price;
+    private String image;
+    @Column(name = "active")
+    private Boolean isAvailable;
 }
